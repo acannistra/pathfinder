fcdeef806bef56f1f61e6bcfd670b794701a7d00
EC2 Default User <ec2-user@ip-172-31-40-152.ec2.internal>
2014-11-23 22:19:44 +0000

    updated pathFinder algorithm, if in path does not repeat. Ensured that a finish occurred

--- src/controller/pathFinder_titlelinks_controller.erl
+++ src/controller/pathFinder_titlelinks_controller.erl
@@ -8,19 +8,64 @@ list('GET', []) ->
 path('GET',[]) ->
   From  = Req:query_param( "from" ),
   To = Req:query_param( "to" ),
-  Titlelinks = next(From),
+  CollectorPid = spawn(fun () -> collector(1,[],self()) end),
+  path_finder(10,CollectorPid,From,To,[]),
+  receive
+	{lists,Titlelinks} ->io:format("here ~p ~n",[Titlelinks])
+  end,
   {ok, [{titlelinks, Titlelinks}]}.
+ % {ok, [{titlelinks, collector(1,[],self())}]}.
 
 
-collector(0, L) -> L;
-collector(Count,L) ->
+collector(0, L,ReturnPid) -> io:format("Done ~nLists: ~p ~n",[L]),ReturnPid ! {lists,L};
+collector(Count,L,ReturnPid) ->
 	receive
-		{spawning,N} -> collector(Count + N - 1, L);
-		{list,NewList} -> collector(Count - 1, [NewList | L])
+		{spawning,PID,N} ->
+			PID ! ok,
+			 collector(Count + N - 1, L,ReturnPid);
+		{list,NewList} -> 
+			collector(Count - 1, [NewList | L],ReturnPid)
 	end.
-	
+
+path_finder(_,Collector_PID,Word,EndWord,Path) 
+	when Word =:= EndWord -> 
+		Collector_PID ! {list,lists:append(Path,[EndWord])};
+path_finder(0,Collector_PID,_,_,_) -> Collector_PID ! {spawning,self(),0};
+path_finder(Remaining,Collector_PID,Word,EndWord,Path) ->
+	Contains = contains(Path,Word),
+	if
+	Contains -> Collector_PID ! {spawning,self(),0};
+	true->  
+	Words = next(Word),
+	Collector_PID ! {spawning,self(),length(Words)},
+	receive
+		ok -> ok
+	end,
+	NewPath = lists:append(Path, [Word]),
+	lists:foreach(fun (W) -> 
+		spawn(fun () -> 
+		path_finder(Remaining - 1, Collector_PID,W,EndWord,NewPath)
+		end)
+		end,
+		Words)
+	end.
 
 next(Word) ->
    Words = boss_db:find(titlelink, [{page_title, 'equals', Word}]),
-   [To || {titlelink,_,From,To} <- Words].
-	
+  [binary_to_list(To) || {titlelink,_,From,To} <- Words].
+
+contains(List, Item) -> length(lists:filter( fun(X) -> X =:= Item end, List)) > 0.
+
+type_of(X) when is_integer(X)   -> integer;
+type_of(X) when is_float(X)     -> float;
+type_of(X) when is_list(X)      -> list;
+type_of(X) when is_tuple(X)     -> tuple;
+type_of(X) when is_bitstring(X) -> bitstring;  % will fail before e12
+type_of(X) when is_binary(X)    -> binary;
+type_of(X) when is_boolean(X)   -> boolean;
+type_of(X) when is_function(X)  -> function;
+type_of(X) when is_pid(X)       -> pid;
+type_of(X) when is_port(X)      -> port;
+type_of(X) when is_reference(X) -> reference;
+type_of(X) when is_atom(X)      -> atom;
+type_of(_X)                     -> unknown.	
